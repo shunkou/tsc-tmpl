55f5ca0c013c3ae9cd3d1822491c1481b59efdbe
tnakamura <tnakamura@iij.ad.jp>
2016-05-27 18:39:24 +0900

    update bootstrap.sh

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 tsc-tmpl
 ========
+
+Install
+-------
+```
+$ curl -L git.io/tsc-tmpl | sh -s <appname>
+```
+
+Typings
+-------
+```
+$ npm run typings install <source>~<name>
+```
+
+Build
+-----
+```
+$ npm run build
+```
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,23 @@
 #!/bin/sh
 
-# temp
+set -e
+
+GITURL=https://github.com/shunkou/tsc-tmpl.git
+
+## set appname
+if [ "$1" = "" ]; then
+  appname=tsc-tmpl
+else
+  appname=$1
+fi
+
+## git clone
+git clone ${GITURL} ${appname}
+cd ${appname}
+
+## npm install
+npm install
+
+## clean git files
+rm -rf .git
+rm .gitignore
