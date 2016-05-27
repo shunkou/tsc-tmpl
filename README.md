87f122c5ea30f24444d351e5f52122c36d9b1418
tetsu <tetsu@hongo.wide.ad.jp>
2016-05-28 03:09:42 +0900

    add 'gulp compress' to minimize js

--- README.md
+++ README.md
@@ -16,5 +16,9 @@ $ npm run typings install <source>~<name>
 Build
 -----
 ```
-$ npm run build
+$ npm run build #=> built/bundle.js
+```
+to minimize
+```
+$ npm run compress #=> built/bundle.min.js
 ```
--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,19 @@
 var gulp = require('gulp')
+    uglify = require('gulp-uglify')
+    rename = require('gulp-rename')
     webpack = require('webpack-stream')
     webpackConfig = require('./webpack.config.js')
     ;
 
-gulp.task('build', function(cb) {
+gulp.task('build', () => {
   return gulp.src('')
     .pipe(webpack(webpackConfig))
     .pipe(gulp.dest(''));
+});
+
+gulp.task('compress', ['build'], () => {
+  return gulp.src('./built/bundle.js')
+    .pipe(uglify())
+    .pipe(rename('bundle.min.js'))
+    .pipe(gulp.dest('./built'));
 });
